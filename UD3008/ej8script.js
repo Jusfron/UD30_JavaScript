ba3c4bbfb78eda48a122ffcea896a4e1d08af3ed
David Marcos <david.13254@gmail.com>
2022-09-01 16:15:54 +0200

    funcion esPar

--- UD3008/ej8script.js
+++ UD3008/ej8script.js
@@ -1,12 +1,16 @@
 let numero;
 numero = window.prompt("Introduce el numero", numero);
 
-if(esImpar(numero)) { 
-    console.log("El numero es impar");
-} else {
-    console.log("El numero es par");
-}
 
-function esImpar(x) { //Devuelve 0 si es par y 1 si es impar
-    return x & 1;    //Ej.   1101 & 0001 ==> 0001 ///   1100 & 0001 ==> 0000
+console.log(esPar(numero));
+
+
+function esPar(x) { //Devuelve 0 si es par y 1 si es impar
+    let impar = x & 1;    //Ej.   1101 & 0001 ==> 0001 ///   1100 & 0001 ==> 0000
+
+    if(impar) {
+        return "El numero es impar";
+    } else {
+        return "El numero es par";
+    }
 }
